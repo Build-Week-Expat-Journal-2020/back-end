ec240f7c21111877477a7517ed7e1fd3ea0b1155
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-11-14 14:36:48 -0600

    wrote migration schema for users and posts

--- data/migrations/20201114130534_init.js
+++ data/migrations/20201114130534_init.js
@@ -1,7 +1,24 @@
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.createTable("users", tbl => {
+    tbl.increments("id");
+    tbl.text("username").notNullable().unique();
+    tbl.text("password").notNullable();
+  });
+
+  await knex.createTable("posts", tbl => {
+    tbl.increments("id");
+    tbl.text("photo").notNullable();
+    tbl.text("story");
+    tbl
+      .integer("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.dropTableIfExists("posts");
+  await knex.dropTableIfExists("users");
 };
